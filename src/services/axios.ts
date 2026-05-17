ec1c8885282cfaeaa8a9a15422319880dbc46d87
Binh <phanngocbinh515@gmail.com>
2026-05-17 22:53:29 +0700

    code FE

---
+++ src/services/axios.ts
@@ -0,0 +1,33 @@
+import axios
+from "axios";
+
+const api = axios.create({
+  baseURL:
+    process.env
+      .NEXT_PUBLIC_API_URL,
+
+  headers: {
+    "Content-Type":
+      "application/json",
+  },
+
+  withCredentials: true,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token =
+      localStorage.getItem(
+        "accessToken",
+      );
+
+    if (token) {
+      config.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return config;
+  },
+);
+
+export default api;
